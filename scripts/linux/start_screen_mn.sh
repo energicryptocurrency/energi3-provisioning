2e95e98a1a6be49baba594e98b76113858a4ade6
Zaki Alam <zaki.alam@energi.team>
2020-02-26 18:41:16 +0000

    Upload New File

---
+++ scripts/linux/start_screen_mn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#####################################################################
+# Description: This script is to start energi core node masternode server within screen 
+#
+# Download this script
+# wget https://raw.githubusercontent.com/energicryptocurrency/energi3-provisioning/master/scripts/linux/start_screen_mn.sh
+#####################################################################
+#
+
+export PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:$HOME/energi3/bin
+
+# Start masternode in screen
+screen -S energi3 start_mn.sh
