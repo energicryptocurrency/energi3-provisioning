fb0ce0210ac9b18737e7a06cb241bab4b895564b
zalam003 <48445346+zalam003@users.noreply.github.com>
2020-03-22 14:26:49 -0400

    Add files via upload

--- scripts/linux/autocollaterize.sh
+++ scripts/linux/autocollaterize.sh
@@ -28,79 +28,6 @@ GREEN=`tput setaf 2`
 YELLOW=`tput setaf 2`
 NC=`tput sgr0`
 
-#
-if [[ -f ${SERVICEFILE} ]]
-then
-    CHKIFTHERE=`grep miner.autocollateralize ${SERVICEFILE}`
-    ISDISABLED=`grep "miner.autocollateralize 0" ${SERVICEFILE}`
-    ISENABLED=`grep "miner.autocollateralize 1" ${SERVICEFILE}`
-
-else
-    echo "Cannot find service file to update"
-    echo "  Filename: ${SERVICEFILE}"
-    echo
-    echo "Exiting script..."
-    echo
-    exit 1
-fi
-
-#
-echo "${GREEN}"
-clear 2> /dev/null
-cat << "ENERGI3"
-      ___       ______ _   _ ______ _____   _____ _____ ____
-     /\  \     |  ____| \ | |  ____|  __ \ / ____|_   _|___ \
-    /::\  \    | |__  |  \| | |__  | |__) | |  __  | |   __) |
-   /:/\:\__\   |  __| | . ` |  __| |  _  /| | |_ | | |  |__ <
-  /:/ /:/ _/_  | |____| |\  | |____| | \ \| |__| |_| |_ ___) |
- /:/ /:/ /\__\ |______|_| \_|______|_|  \_\\_____|_____|____/
- \:\ \/ /:/  /
-ENERGI3
-echo "${GREEN}  \:\  /:/  /  ${NC}This script determines if autocollateralize is set"
-echo "${GREEN}   \:\/:/  /   ${NC}and changes the setting for you. You do not need to"
-echo "${GREEN}    \::/  /    ${NC}re-announce masternode or unlock staking after you"
-echo "${GREEN}     \/__/     ${NC}run the script."
-echo ${NC}
-echo
-
-if [[ ! -z ${ISDISABLED} ]]
-then
-    echo "autocollateralize is ${RED}NOT SET${NC} on the Core Node"
-    echo
-    echo "To set autocollateralize, type: s"
-    echo "Exit without changing, type   : x"
-    echo
-    read -n 1 -p " Select option (s/[x]): " OPTION
-    echo
-    OPTION=${OPTION,,}    # tolower
-    echo
-
-elif [[ ! -z ${ISENABLED} ]]
-then
-    echo "autocollateralize is ${GREEN}SET${NC} on the Core Node"
-    echo
-    echo "Remove autocollateralize, type: r"
-    echo "Exit without changing, type   : x"
-    echo
-    read -n 1 -p " Select option (r/[x]): " OPTION
-    echo
-    OPTION=${OPTION,,}    # tolower
-    echo
-
-elif [[ -z ${CHKIFTHERE} ]]
-then
-    echo "autocollateralize ${BLUE}PARAMETER NOT ENTERED${NC} on the Core Node"
-    echo
-    echo "To set autocollateralize, type: s"
-    echo "Exit without changing, type   : x"
-    echo
-    read -n 1 -p " Select option (s/[x]): " OPTION
-    echo
-    OPTION=${OPTION,,}    # tolower
-    echo
-
-fi
-
 # Command functions
 _cmd-collChk () {
     # Check status of autocollateralize
@@ -129,47 +56,145 @@ _cmd-collOn () {
     _cmd-collChk
 }
 
-case ${OPTION} in
-    s)
-      if [[ -z ${CHKIFTHERE} ]]
-      then
-          _cmd-collOn
-	  echo
-	  echo "Updating energi3.service file for reboots"
-          sudo sed -i 's/node/node --miner.autocollateralize 1/' ${SERVICEFILE}
-	  sudo systemctl daemon-reload
+#
+if [[ -f ${SERVICEFILE} ]]
+then
+    CHKIFTHERE=`grep miner.autocollateralize ${SERVICEFILE}`
+    ISDISABLED=`grep "miner.autocollateralize 0" ${SERVICEFILE}`
+    ISENABLED=`grep "miner.autocollateralize 1" ${SERVICEFILE}`
 
-      elif [[ -z ${ISENABLED} ]]
+else
+    clear
+    echo
+    echo "Cannot find energi3.service file to update"
+    echo "  Filename: ${SERVICEFILE}"
+    echo
+    echo "Exiting script..."
+    echo
+    echo "To manually change setting attach to Core Node:"
+    echo "   energi3 attach"
+    echo
+    echo "and then run:"
+    echo "   miner.setAutocollateralize(0) - to disable"
+    echo "   miner.setAutocollateralize(1) - to enable"
+    echo "   miner.setAutocollateralize()  - to check current setting"
+    echo
+    exit 1
+fi
+
+#
+echo "${GREEN}"
+clear 2> /dev/null
+cat << "ENERGI3"
+      ___       ______ _   _ ______ _____   _____ _____ ____
+     /\  \     |  ____| \ | |  ____|  __ \ / ____|_   _|___ \
+    /::\  \    | |__  |  \| | |__  | |__) | |  __  | |   __) |
+   /:/\:\__\   |  __| | . ` |  __| |  _  /| | |_ | | |  |__ <
+  /:/ /:/ _/_  | |____| |\  | |____| | \ \| |__| |_| |_ ___) |
+ /:/ /:/ /\__\ |______|_| \_|______|_|  \_\\_____|_____|____/
+ \:\ \/ /:/  /
+ENERGI3
+echo "${GREEN}  \:\  /:/  /  ${NC}This script determines if autocollateralize is enabled"
+echo "${GREEN}   \:\/:/  /   ${NC}and changes the setting for you. You do not need to"
+echo "${GREEN}    \::/  /    ${NC}re-announce masternode or unlock staking after you"
+echo "${GREEN}     \/__/     ${NC}run the script."
+echo ${NC}
+echo
+
+if [[ ! -z ${ISDISABLED} ]]
+then
+    echo "autocollateralize is ${RED}DISABLED{NC} on the Core Node"
+    echo
+    echo "To enable autocollateralize, type: e"
+    echo "Exit without making change, type : x"
+    echo
+    read -n 1 -p " Select option (e/[x]): " OPTION
+    echo
+    OPTION=${OPTION,,}    # tolower
+    echo
+
+elif [[ ! -z ${ISENABLED} ]]
+then
+    echo "autocollateralize is ${GREEN}ENABLED${NC} on the Core Node"
+    echo
+    echo "To diable autocollateralize, type: d"
+    echo "Exit without making change, type : x"
+    echo
+    read -n 1 -p " Select option (d/[x]): " OPTION
+    echo
+    OPTION=${OPTION,,}    # tolower
+    echo
+
+elif [[ -z ${CHKIFTHERE} ]]
+then
+    # Blank if parameter is not there
+    echo "autocollateralize ${BLUE}PARAMETER NOT ENTERED${NC} on the Core Node"
+    _cmd-collChk
+    echo
+    echo "To enable autocollateralize, type: e"
+    echo "To diable autocollateralize, type: d"
+    echo "Exit without making change, type : x"
+    echo
+    read -n 1 -p " Select option (e/d/[x]): " OPTION
+    echo
+    OPTION=${OPTION,,}    # tolower
+    echo
+
+fi
+
+
+# Main program
+case ${OPTION} in
+    e)
+      if [[ ! -z ${ISDISABLED} ]]
       then
-          _cmd-collOn
-	  echo
-	  echo "Updating energi3.service file for reboots"
+          echo "Updating energi3.service file for reboots"
           sudo sed -i 's/--miner.autocollateralize 0/--miner.autocollateralize 1/' ${SERVICEFILE}
           sudo systemctl daemon-reload
+          echo
+          _cmd-collOn
+          echo
+          
+      elif [[ -z ${CHKIFTHERE} ]]
+      then
+          # Parameter not there, add to enable
+
+          echo
+          echo "Updating energi3.service file for reboots"
+          sudo sed -i 's/node/node --miner.autocollateralize 1/' ${SERVICEFILE}
+          sudo systemctl daemon-reload
+          echo
+          _cmd-collOn
+          echo
+          
       else
           echo "autocolateralise is already set to roll up"
           echo "No changes made"
+          echo
+          
       fi
 
       ;;
 
-    r)
-      if [[ -z ${CHKIFTHERE} ]]
+    d)
+      if [[ ! -z ${ISENABLED} ]]
       then
-          _cmd-collOff
-	  echo
-	  echo "Updating energi3.service file for reboots"
-          sudo sed -i 's/node/node --miner.autocollateralize 0/' ${SERVICEFILE}
-          sudo systemctl daemon-reload
-
-      elif [[ -z ${ISDISABLED} ]]
-      then
-          _cmd-collOff
-	  echo
-	  echo "Updating energi3.service file for reboots"
+          echo "Updating energi3.service file for reboots"
           sudo sed -i 's/--miner.autocollateralize 1/--miner.autocollateralize 0/' ${SERVICEFILE}
           sudo systemctl daemon-reload
-
+          echo
+          _cmd-collOff
+          echo
+          
+      elif [[ -z ${CHKIFTHERE} ]]
+      then
+          echo "Updating energi3.service file for reboots"
+          sudo sed -i 's/node/node --miner.autocollateralize 0/' ${SERVICEFILE}
+          sudo systemctl daemon-reload
+          echo
+          _cmd-collOff
+          echo
+      
       fi
 
       ;;
