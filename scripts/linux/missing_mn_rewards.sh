35fcd3f01f9e57fcb9657bc47f97efb1622d64c0
zalam003 <48445346+zalam003@users.noreply.github.com>
2021-05-16 18:12:16 -0400

    Update missing_mn_rewards.sh

--- scripts/linux/missing_mn_rewards.sh
+++ scripts/linux/missing_mn_rewards.sh
@@ -13,8 +13,8 @@
 : '
 # Run the script to get started:
 ```
-bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/energicryptocurrency/energi3-provisioning/master/scripts/linux/missing_mn_rewards.sh START_BLOCK END_BLOCK MN_ADDR)" ; source ~/.bashrc
-Syntax: missing_mn_rewards.sh START_BLOCK END_BLOCK MN_ADDR
+bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/energicryptocurrency/energi3-provisioning/master/scripts/linux/missing_mn_rewards.sh0)" '' START_BLOCK END_BLOCK MN_ADDR ; source ~/.bashrc
+
 Script arguments:
     START_BLOCK  : Block number to start from
     END_BLOCK    : Block number to stop at
